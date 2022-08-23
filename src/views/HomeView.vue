df228eb7b1859f02a733e184c32559b23a68f21a
Ben Hong <ben@bencodezen.io>
2022-08-23 10:24:02 -0700

    refactor: move reusable parts to external modules

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
-const restaurantStatusList = [
-  'Want to Try',
-  'Recommended',
-  'Do Not Recommend',
-  'Must Try'
-] as const
-
-type RecommendStatus = typeof restaurantStatusList[number]
+import type { Dish, RecommendStatus } from '../types'
+import { restaurantStatusList } from '../constants'
 
 interface Restaurant {
   name?: string
   status?: RecommendStatus
   dishes?: Dish[]
 }
-
-interface Dish {
-  name: string
-  diet?: Diet
-  status?: RecommendStatus
-}
-
-type Diet =
-  | 'vegetarian'
-  | 'vegan'
-  | 'gluten-free'
-  | 'pescatarian'
-  | 'lactose-free'
-  | 'other'
 
 const restaurantList = ref<Restaurant[]>([])
 const newRestaurant = ref<Restaurant>({
